817f97ea8f3ab6d495f76c997e68dd31f90ce03a
alkasehrawat <146313481+alkasehrawat@users.noreply.github.com>
2025-02-19 19:24:00 +0530

    Update pattern13.java

--- pattern13.java
+++ pattern13.java
@@ -7,7 +7,7 @@ public class pattern13 {
                System.out.print("*");
             }
             for(int j=1;j<=2*(n-i);j++){ 
-                System.out.print("-");               
+                System.out.print(" ");               
             }
             for(int j=1;j<=i;j++){
                 System.out.print("*");
@@ -20,7 +20,7 @@ public class pattern13 {
                System.out.print("*");
             }
             for(int j=1;j<=2*(n-i);j++){ 
-                System.out.print("-");               
+                System.out.print(" ");               
             }
             for(int j=i;j>=1;j--){
                 System.out.print("*");
