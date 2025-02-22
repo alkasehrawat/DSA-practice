3368567faec6ec4b7b8c4f546e206729fa2081c3
alkasehrawat <146313481+alkasehrawat@users.noreply.github.com>
2025-02-22 11:57:14 +0530

    Create pattern16.java

---
+++ pattern16.java
@@ -0,0 +1,15 @@
+//pattern
+public class pattern16 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 2 * (n - i + 1) - 1; j >= 1; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
