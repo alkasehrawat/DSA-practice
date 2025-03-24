b882e39fab81fdf6bb6879f56e8251bb944581fb
Alka Sehrawat <146313481+alkasehrawat@users.noreply.github.com>
2025-03-24 23:18:24 +0530

    Update checkValOccurTwice.java

--- checkValOccurTwice.java
+++ checkValOccurTwice.java
@@ -1,4 +1,4 @@
-//if value occurs twice in array return true else false
+//if value occurs twice return true else false
 public class checkValOccurTwice {
     public static boolean checkTwi(int[] arr){
         for(int i=0;i<arr.length;i++){
