85ab8265be0abf6597599f3baf92c1755a498021
Alka Sehrawat <146313481+alkasehrawat@users.noreply.github.com>
2025-03-26 19:42:04 +0530

    Create print1toNByR.java

---
+++ print1toNByR.java
@@ -0,0 +1,13 @@
+// To print 1 to N numbers using recursion
+public class print1toNByR {
+    public static void print1toN(int n){
+        if(n==0)return;
+        print1toN( n-1);
+        System.out.print(n+" ");
+
+    }
+    public static void main(String[] args) {
+        int n=5;
+        print1toN(n);
+    }
+}
